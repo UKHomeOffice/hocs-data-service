c221c21b83d2701d10fe9b4cf26bca0f56c6bde8
nickmcmahonHOD <nick.mcmahon@digital.homeoffice.gov.uk>
2018-01-08 12:43:17 +0000

    exclude /healthz from security

--- src/main/java/uk/gov/digital/ho/hocs/SecurityConfiguration.java
+++ src/main/java/uk/gov/digital/ho/hocs/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -34,6 +35,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and().httpBasic()
                 .authenticationEntryPoint(authEntryPoint);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/healthz");
     }
 
     @Autowired
