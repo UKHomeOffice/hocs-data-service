a9e8a2e2cc11fb7b2d07efaa14f491f74517b75d
nickmcmahonHOD <nick.mcmahon@digital.homeoffice.gov.uk>
2018-01-08 10:49:40 +0000

    Add test profile

--- src/test/java/uk/gov/digital/ho/hocs/lists/DataListResourceIntTest.java
+++ src/test/java/uk/gov/digital/ho/hocs/lists/DataListResourceIntTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 import uk.gov.digital.ho.hocs.lists.dto.DataListEntityRecord;
@@ -24,6 +25,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
+@ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 public class DataListResourceIntTest {
