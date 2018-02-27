de17d141a6a6fe437874fb2706e8072831f7d8ac
nickmcmahonHOD <nick.mcmahon@digital.homeoffice.gov.uk>
2018-02-27 12:49:28 +0000

    remove unneeded dep

--- src/main/java/uk/gov/digital/ho/hocs/house/ingest/EuropeMember.java
+++ src/main/java/uk/gov/digital/ho/hocs/house/ingest/EuropeMember.java
@@ -2,8 +2,10 @@ package uk.gov.digital.ho.hocs.house.ingest;
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 public class EuropeMember {
 
     @JacksonXmlProperty(localName = "fullName")
--- src/main/java/uk/gov/digital/ho/hocs/house/ingest/ScottishMember.java
+++ src/main/java/uk/gov/digital/ho/hocs/house/ingest/ScottishMember.java
@@ -3,9 +3,11 @@ package uk.gov.digital.ho.hocs.house.ingest;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @AllArgsConstructor
+@NoArgsConstructor
 public class ScottishMember {
 
     @JsonProperty(value = "ParliamentaryName")
