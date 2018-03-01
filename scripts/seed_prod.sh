e88acc7a0fd6f47d69c5b679dc14dca4fae81b0e
lucascairns <lucas.cairns@digital.homeoffice.gov.uk>
2018-03-01 10:49:06 +0000

    Updated seed data prod script

--- scripts/seed_prod.sh
+++ scripts/seed_prod.sh
@@ -48,39 +48,39 @@ echo "Pulling member information from external API"
 curl -u $user:$pass $target_uri/houses/refresh
 
 echo "Posting Users"
-curl -u $user:$pass -X POST $target_uri/users/dept/BICS -F "file=@$data_dir/users/BICS.csv" \
- -H "Content-Type: multipart/form-data"
-curl -u $user:$pass -X POST $target_uri/users/dept/CPFG -F "file=@$data_dir/users/CPFG.csv" \
- -H "Content-Type: multipart/form-data"
-curl -u $user:$pass -X POST $target_uri/users/dept/CR -F "file=@$data_dir/users/CR.csv" \
- -H "Content-Type: multipart/form-data"
-curl -u $user:$pass -X POST $target_uri/users/dept/DCU -F "file=@$data_dir/users/DCU.csv" \
- -H "Content-Type: multipart/form-data"
-curl -u $user:$pass -X POST $target_uri/users/dept/FOI -F "file=@$data_dir/users/FOI.csv" \
- -H "Content-Type: multipart/form-data"
-curl -u $user:$pass -X POST $target_uri/users/dept/HMPO_CCC -F "file=@$data_dir/users/HMPO_CCC.csv" \
- -H "Content-Type: multipart/form-data"
-curl -u $user:$pass -X POST $target_uri/users/dept/HMPO_Collectives -F "file=@$data_dir/users/HMPO_Collectives.csv" \
- -H "Content-Type: multipart/form-data"
-curl -u $user:$pass -X POST $target_uri/users/dept/HMPO_FOI -F "file=@$data_dir/users/HMPO_FOI.csv" \
- -H "Content-Type: multipart/form-data"
-curl -u $user:$pass -X POST $target_uri/users/dept/HO_Legal_Advisors -F "file=@$data_dir/users/HO_Legal_Adviser.csv" \
- -H "Content-Type: multipart/form-data"
-curl -u $user:$pass -X POST $target_uri/users/dept/Immigration_Enforcement -F "file=@$data_dir/users/Immigration_Enforcement.csv" \
- -H "Content-Type: multipart/form-data"
-curl -u $user:$pass -X POST $target_uri/users/dept/OSCT -F "file=@$data_dir/users/OSCT.csv" \
- -H "Content-Type: multipart/form-data"
-curl -u $user:$pass -X POST $target_uri/users/dept/Private_Office -F "file=@$data_dir/users/Private_Office.csv" \
- -H "Content-Type: multipart/form-data"
-curl -u $user:$pass -X POST $target_uri/users/dept/UKVI_ALS -F "file=@$data_dir/users/UKVI_ALS.csv" \
- -H "Content-Type: multipart/form-data"
-curl -u $user:$pass -X POST $target_uri/users/dept/UKVI_CCT -F "file=@$data_dir/users/UKVI_CCT.csv" \
- -H "Content-Type: multipart/form-data"
-curl -u $user:$pass -X POST $target_uri/users/dept/UKVI_RCM -F "file=@$data_dir/users/UKVI_RCM.csv" \
- -H "Content-Type: multipart/form-data"
-curl -u $user:$pass -X POST $target_uri/users/dept/UKVI_ROS -F "file=@$data_dir/users/UKVI_ROS.csv" \
- -H "Content-Type: multipart/form-data"
-curl -u $user:$pass -X POST $target_uri/users/dept/UKVI -F "file=@$data_dir/users/UKVI.csv" \
+#curl -u $user:$pass -X POST $target_uri/users/dept/BICS -F "file=@$data_dir/users/BICS.csv" \
+# -H "Content-Type: multipart/form-data"
+#curl -u $user:$pass -X POST $target_uri/users/dept/CPFG -F "file=@$data_dir/users/CPFG.csv" \
+# -H "Content-Type: multipart/form-data"
+#curl -u $user:$pass -X POST $target_uri/users/dept/CR -F "file=@$data_dir/users/CR.csv" \
+# -H "Content-Type: multipart/form-data"
+#curl -u $user:$pass -X POST $target_uri/users/dept/DCU -F "file=@$data_dir/users/DCU.csv" \
+# -H "Content-Type: multipart/form-data"
+#curl -u $user:$pass -X POST $target_uri/users/dept/FOI -F "file=@$data_dir/users/FOI.csv" \
+# -H "Content-Type: multipart/form-data"
+#curl -u $user:$pass -X POST $target_uri/users/dept/HMPO_CCC -F "file=@$data_dir/users/HMPO_CCC.csv" \
+# -H "Content-Type: multipart/form-data"
+#curl -u $user:$pass -X POST $target_uri/users/dept/HMPO_Collectives -F "file=@$data_dir/users/HMPO_Collectives.csv" \
+# -H "Content-Type: multipart/form-data"
+#curl -u $user:$pass -X POST $target_uri/users/dept/HMPO_FOI -F "file=@$data_dir/users/HMPO_FOI.csv" \
+# -H "Content-Type: multipart/form-data"
+#curl -u $user:$pass -X POST $target_uri/users/dept/HO_Legal_Advisors -F "file=@$data_dir/users/HO_Legal_Adviser.csv" \
+# -H "Content-Type: multipart/form-data"
+#curl -u $user:$pass -X POST $target_uri/users/dept/Immigration_Enforcement -F "file=@$data_dir/users/Immigration_Enforcement.csv" \
+# -H "Content-Type: multipart/form-data"
+#curl -u $user:$pass -X POST $target_uri/users/dept/OSCT -F "file=@$data_dir/users/OSCT.csv" \
+# -H "Content-Type: multipart/form-data"
+#curl -u $user:$pass -X POST $target_uri/users/dept/Private_Office -F "file=@$data_dir/users/Private_Office.csv" \
+# -H "Content-Type: multipart/form-data"
+#curl -u $user:$pass -X POST $target_uri/users/dept/UKVI_ALS -F "file=@$data_dir/users/UKVI_ALS.csv" \
+# -H "Content-Type: multipart/form-data"
+#curl -u $user:$pass -X POST $target_uri/users/dept/UKVI_CCT -F "file=@$data_dir/users/UKVI_CCT.csv" \
+# -H "Content-Type: multipart/form-data"
+#curl -u $user:$pass -X POST $target_uri/users/dept/UKVI_RCM -F "file=@$data_dir/users/UKVI_RCM.csv" \
+# -H "Content-Type: multipart/form-data"
+#curl -u $user:$pass -X POST $target_uri/users/dept/UKVI_ROS -F "file=@$data_dir/users/UKVI_ROS.csv" \
+# -H "Content-Type: multipart/form-data"
+#curl -u $user:$pass -X POST $target_uri/users/dept/UKVI -F "file=@$data_dir/users/UKVI.csv" \
  -H "Content-Type: multipart/form-data"
 
 if [ -n "$alf_uri" ]
@@ -104,23 +104,23 @@ then
     curl -u $user:$pass $target_uri/units/publish
 
 
-    echo "Publishing test users to Alfresco"
-    curl -u $user:$pass $target_uri/users/dept/BICS/publish
-    curl -u $user:$pass $target_uri/users/dept/CPFG/publish
-    curl -u $user:$pass $target_uri/users/dept/CR/publish
-    curl -u $user:$pass $target_uri/users/dept/DCU/publish
-    curl -u $user:$pass $target_uri/users/dept/FOI/publish
-    curl -u $user:$pass $target_uri/users/dept/HMPO_CCC/publish
-    curl -u $user:$pass $target_uri/users/dept/HMPO_Collectives/publish
-    curl -u $user:$pass $target_uri/users/dept/HMPO_FOI/publish
-    curl -u $user:$pass $target_uri/users/dept/HO_Legal_Advisors/publish
-    curl -u $user:$pass $target_uri/users/dept/Immigration_Enforcement/publish
-    curl -u $user:$pass $target_uri/users/dept/OSCT/publish
-    curl -u $user:$pass $target_uri/users/dept/Private_Office/publish
-    curl -u $user:$pass $target_uri/users/dept/UKVI_ALS/publish
-    curl -u $user:$pass $target_uri/users/dept/UKVI_CCT/publish
-    curl -u $user:$pass $target_uri/users/dept/UKVI_RCM/publish
-    curl -u $user:$pass $target_uri/users/dept/UKVI_ROS/publish
-    curl -u $user:$pass $target_uri/users/dept/UKVI/publish
+    echo "Publishing users to Alfresco"
+#    curl -u $user:$pass $target_uri/users/dept/BICS/publish
+#    curl -u $user:$pass $target_uri/users/dept/CPFG/publish
+#    curl -u $user:$pass $target_uri/users/dept/CR/publish
+#    curl -u $user:$pass $target_uri/users/dept/DCU/publish
+#    curl -u $user:$pass $target_uri/users/dept/FOI/publish
+#    curl -u $user:$pass $target_uri/users/dept/HMPO_CCC/publish
+#    curl -u $user:$pass $target_uri/users/dept/HMPO_Collectives/publish
+#    curl -u $user:$pass $target_uri/users/dept/HMPO_FOI/publish
+#    curl -u $user:$pass $target_uri/users/dept/HO_Legal_Advisors/publish
+#    curl -u $user:$pass $target_uri/users/dept/Immigration_Enforcement/publish
+#    curl -u $user:$pass $target_uri/users/dept/OSCT/publish
+#    curl -u $user:$pass $target_uri/users/dept/Private_Office/publish
+#    curl -u $user:$pass $target_uri/users/dept/UKVI_ALS/publish
+#    curl -u $user:$pass $target_uri/users/dept/UKVI_CCT/publish
+#    curl -u $user:$pass $target_uri/users/dept/UKVI_RCM/publish
+#    curl -u $user:$pass $target_uri/users/dept/UKVI_ROS/publish
+#    curl -u $user:$pass $target_uri/users/dept/UKVI/publish
 
 fi
